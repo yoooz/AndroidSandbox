e8ed739589d369c053c0ab693dd66b958de7eeec
yoooz <oor.9@outlook.com>
2021-01-19 00:00:52 +0900

    install android-target-tooltip

--- app/src/main/java/com/example/livedatasample/BlankFragment.kt
+++ app/src/main/java/com/example/livedatasample/BlankFragment.kt
@@ -1,12 +1,16 @@
 package com.example.livedatasample
 
+import android.graphics.Typeface
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.livedatasample.databinding.FragmentBlankBinding
+import it.sephiroth.android.library.xtooltip.ClosePolicy
+import it.sephiroth.android.library.xtooltip.Tooltip
 
 /**
  * A simple [Fragment] subclass.
@@ -17,10 +21,12 @@ class BlankFragment : Fragment() {
     private var _binding: FragmentBlankBinding? = null
     private val binding get() = _binding!!
 
+    private var toolTip: Tooltip? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         super.onCreateView(inflater, container, savedInstanceState)
 
         _binding = FragmentBlankBinding.inflate(inflater, container, false)
@@ -33,5 +39,27 @@ class BlankFragment : Fragment() {
         binding.button.setOnClickListener {
             findNavController().navigate(R.id.action_blankFragment_to_mainFragment)
         }
+
+        binding.root.post { showToolTip() }
+    }
+
+    private fun showToolTip() {
+        toolTip = Tooltip.Builder(requireContext())
+            .anchor(binding.button, 0, 0, true)
+            .text("tooltip")
+            .typeface(Typeface.DEFAULT_BOLD)
+            .arrow(true)
+            .floatingAnimation(Tooltip.Animation.DEFAULT)
+            .closePolicy(ClosePolicy.TOUCH_ANYWHERE_CONSUME)
+            .overlay(false)
+            .create()
+
+        toolTip
+            ?.doOnShown { Toast.makeText(requireContext(), "show tool tip", Toast.LENGTH_SHORT).show() }
+            ?.doOnHidden {
+                binding.button.callOnClick()
+                Toast.makeText(requireContext(), "hidden tool tip", Toast.LENGTH_SHORT).show()
+            }
+            ?.show(binding.button, Tooltip.Gravity.BOTTOM, true)
     }
 }
